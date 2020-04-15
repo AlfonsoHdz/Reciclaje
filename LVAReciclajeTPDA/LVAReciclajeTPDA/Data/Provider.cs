e919e323b3238347169dc3ff90bbc717257d1b95
AlfonsoHdz <alfonsinic2013@gmail.com>
2020-04-14 20:35:42 -0500

    Creacion de la clase Provider

---
+++ LVAReciclajeTPDA/LVAReciclajeTPDA/Data/Provider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace LVAReciclajeTPDA.Data
+{
+    public class Provider
+    {
+        public int Id { get; set; }
+
+        public string EnterpriseName { get; set; }
+        public string Brand { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Mail { get; set; }
+
+        public ICollection<Purchase> Purchases { get; set; }
+        public ICollection<PurchaseDetail> PurchaseDetails { get; set; }
+
+    }
+}
