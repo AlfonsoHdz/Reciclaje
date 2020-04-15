5dbad170e729c7b75710ff53976a4ca12c574d01
AlfonsoHdz <alfonsinic2013@gmail.com>
2020-04-14 20:29:10 -0500

    Creacion DataContext

---
+++ LVAReciclajeTPDA/LVAReciclajeTPDA/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿namespace LVAReciclajeTPDA.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base ("name = con")
+        {
+
+        }
+
+        DbSet<PurchaseDetail> PurchaseDetails { get; set; }
+        DbSet<Provider> Providers { get; set; }
+        DbSet<Client> Clients { get; set; }
+
+
+
+    }
+}
