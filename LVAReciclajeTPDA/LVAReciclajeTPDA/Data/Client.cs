c14ef794b836f0ea049d35598c1efcd114e90744
AlfonsoHdz <alfonsinic2013@gmail.com>
2020-04-14 20:23:50 -0500

    Cracion de la clase Client

---
+++ LVAReciclajeTPDA/LVAReciclajeTPDA/Data/Client.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace LVAReciclajeTPDA.Data
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surnames { get; set; }
+        public string Rfc { get; set; }
+        public string Curp { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public string Contact { get; set; }
+
+        public ICollection<Purchase> Purchases { get; set; }
+    }
+}
